a2794e4db02f2df71b7acc1168830ab26de43e21
sujitha <sujitha@gamil.com>
2024-09-30 19:42:32 +0530

    functions

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -3,4 +3,4 @@
 echo "all variables passed to the script: $@"
 echo "number of varaibles passed: $#"
 echo "script name: $0"
-eho "current working directory: $pwd"
+ehco "current working directory: $pwd"
