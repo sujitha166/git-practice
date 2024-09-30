139c5a43a5a45dcdfeac935e988a7b0f4913dc6b
sujitha <sujitha@gamil.com>
2024-09-30 19:36:58 +0530

    functions

--- 04-array.sh
+++ 04-array.sh
@@ -2,9 +2,9 @@
 
 fruits=("apple kiwi banana")
 
-echo "first fruit is:${fruits[0]}"
-echo "second fruit is:${fruits[1]}"
-echo "third fruit is :${fruits[2]}"
+echo "first fruit is: ${fruits[0]}"
+echo "second fruit is: ${fruits[1]}"
+echo "third fruit is: ${fruits[2]}"
 
 
-echo "first fruit is :${fruits[@]}"
+echo "first fruit is: ${fruits[@]}"
