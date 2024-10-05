86d9d42e3056965052de1d78acdce12a7f1203df
sujitha <sujitha@gamil.com>
2024-10-05 12:27:48 +0530

    variables

---
+++ 011-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1::how are you?"
+echo "$PERSON2::iam fine how do you do $PERSON1"
+echo "$PERSON1h::iam also good is everything fine"
+echo "$PERSON2::yah $PERSON1 good...."
