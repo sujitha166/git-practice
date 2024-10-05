067a5b306a8f320638de00d8e477621ccc77fdd3
sujitha <sujitha@gamil.com>
2024-10-05 19:35:16 +0530

    variables

--- 13-installation.sh
+++ 13-installation.sh
@@ -11,4 +11,20 @@ then
 
 fi
 
-dnf install mysql -y
+dnf installed git
+
+if [ $? ne 0 ]
+then 
+   echo "git is not installed,going to install"
+   dnf install git -y
+   if [ $? -ne 0 ]
+   then
+      echo "git installation is not success...check it"
+      exit 1
+  else
+    echo "git installation is success"
+  fi
+  else 
+     echo "git is already installed nothing to do.."
+     fi
+           
