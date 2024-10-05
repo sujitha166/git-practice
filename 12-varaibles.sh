599aa964730d03c8d310a533a18d3df96cb9e89c
sujitha <sujitha@gamil.com>
2024-10-05 13:27:05 +0530

    variables

---
+++ 12-varaibles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::how are you?"
+echo "$PERSON2::iam fine how do you do $PERSON1"
+echo "$PERSON1::iam also good is everything fine"
+echo "$PERSON2::yah $PERSON1 good...."
